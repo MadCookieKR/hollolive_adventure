7a4c2ab040b475b166d776737ab9b47f18127e77
DESKTOP-I2OGSEQ\manta <manta11246>
2021-12-01 01:20:44 +0900

    AppTransform 추가

---
+++ Assets/script/AppTransform.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using UnityEngine;
+
+public class AppTransform
+{
+
+    private Transform transform;
+
+    public AppTransform(Transform transform)
+    {
+        this.transform = transform;
+    }
+
+    public IEnumerator moveSmooth(Vector2 target, float speed)
+    {
+        while (true)
+        {
+            Vector2 oldPosition = transform.position;
+            float step = speed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, target, step);
+            if(Vector2.Distance(transform.position, oldPosition) < 0.1f)
+            {
+                break;
+            }
+            yield return new WaitForFixedUpdate();
+        }
+
+
+    }
+}
--- Assets/script/CharacterAnimator.cs
+++ Assets/script/CharacterAnimator.cs
@@ -6,12 +6,16 @@ public class CharacterAnimator : MonoBehaviour
 {
     public SpriteRenderer spriteRenderer;
     private FadeOut fadeOut;
+    private AppTransform appTransform;
 
     // Start is called before the first frame update
     void Start()
     {
         fadeOut = new FadeOut(spriteRenderer);
-        StartCoroutine(fadeOut.start(0.5f));     
+        appTransform = new AppTransform(GetComponent<Transform>());
+        //StartCoroutine(fadeOut.start(0.5f));
+        StartCoroutine(appTransform.moveSmooth(new Vector2(10, 0), 10));
+           
     }
 
     // Update is called once per frame
