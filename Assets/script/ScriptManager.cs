643cc35b03c14a25f55f060f13e84beb0adad9ce
DESKTOP-I2OGSEQ\manta <manta11246>
2021-12-01 01:48:56 +0900

    스크립트 분기시스템 짬

--- Assets/script/ScriptManager.cs
+++ Assets/script/ScriptManager.cs
@@ -7,12 +7,15 @@ using System.Linq;
 public class ScriptManager : MonoBehaviour
 {
     public TextVisualizer textVisualizer;
-    public TextAsset text;
+    private string actFolderPath = "A1";
+    private int currentScriptIndex = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        textVisualizer.scripts = text.text.Split('\n').ToList();        
+        string scriptFilePath = "Assets/InGameScript/" + actFolderPath + "/" + currentScriptIndex + ".txt";
+
+        textVisualizer.scripts = File.ReadAllLines(scriptFilePath).ToList();        
     }
 
 }
